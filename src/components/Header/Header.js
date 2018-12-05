0d22498ee1b67f0ba77f3e830d92179ddc523b00
Muhammad Rizal <muhzal94@gmail.com>
2018-12-05 23:49:03 +0700

    Add Header Components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Header.scss";
+
+const Header = (props) => {
+  return (
+    <nav>
+      <div className="container">
+        <a className="logo" href="/">
+        </a>
+        <button className="menu">
+        </button>
+      </div>
+    </nav>
+  )
+}
+
+export default Header;
