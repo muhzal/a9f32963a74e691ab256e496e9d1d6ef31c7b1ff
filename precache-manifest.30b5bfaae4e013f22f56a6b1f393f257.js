19c5155550fb2535e9227d4498e8073cf7dee3e0
Muhammad Rizal <muhzal94@gmail.com>
2018-12-06 03:28:56 +0700

    Updates

---
+++ precache-manifest.30b5bfaae4e013f22f56a6b1f393f257.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c68fb1c33d3daa5a2746a76c4c7aaa6",
+    "url": "/a9f32963a74e691ab256e496e9d1d6ef31c7b1ff/static/media/lunch-1.8c68fb1c.jpg"
+  },
+  {
+    "revision": "8e5c23fdd27a182bc4ca",
+    "url": "/a9f32963a74e691ab256e496e9d1d6ef31c7b1ff/static/css/main.e7886489.chunk.css"
+  },
+  {
+    "revision": "af157aebf8ef11e02b08",
+    "url": "/a9f32963a74e691ab256e496e9d1d6ef31c7b1ff/static/js/1.af157aeb.chunk.js"
+  },
+  {
+    "revision": "6c94b4b4811fa7b86696",
+    "url": "/a9f32963a74e691ab256e496e9d1d6ef31c7b1ff/static/js/runtime~main.6c94b4b4.js"
+  },
+  {
+    "revision": "30121a10834e5904f7e34e8c75d38af5",
+    "url": "/a9f32963a74e691ab256e496e9d1d6ef31c7b1ff/static/media/lunch.30121a10.svg"
+  },
+  {
+    "revision": "0cdf1fbe128d7bc52e86571afe30fc14",
+    "url": "/a9f32963a74e691ab256e496e9d1d6ef31c7b1ff/static/media/location.0cdf1fbe.svg"
+  },
+  {
+    "revision": "b6b6c4db699010301b02593710377f70",
+    "url": "/a9f32963a74e691ab256e496e9d1d6ef31c7b1ff/static/media/chevron-right-solid.b6b6c4db.svg"
+  },
+  {
+    "revision": "8e5c23fdd27a182bc4ca",
+    "url": "/a9f32963a74e691ab256e496e9d1d6ef31c7b1ff/static/js/main.8e5c23fd.chunk.js"
+  },
+  {
+    "revision": "8782ded9b3afe7822aff4405ec2b65c4",
+    "url": "/a9f32963a74e691ab256e496e9d1d6ef31c7b1ff/static/media/lunch-2.8782ded9.jpg"
+  },
+  {
+    "revision": "bc628e9663342141311e8a2dcca6a166",
+    "url": "/a9f32963a74e691ab256e496e9d1d6ef31c7b1ff/static/media/lunch-3.bc628e96.jpg"
+  },
+  {
+    "revision": "c7feaff0600c16efcd74479a24b8837c",
+    "url": "/a9f32963a74e691ab256e496e9d1d6ef31c7b1ff/static/media/lunch-4.c7feaff0.jpg"
+  },
+  {
+    "revision": "5f7dd9c3e37f578e6c7517bd1c9b991c",
+    "url": "/a9f32963a74e691ab256e496e9d1d6ef31c7b1ff/static/media/lunch-5.5f7dd9c3.jpg"
+  },
+  {
+    "revision": "c275e5189ecaabdbfdc5407dc031c28b",
+    "url": "/a9f32963a74e691ab256e496e9d1d6ef31c7b1ff/static/media/menu.c275e518.svg"
+  },
+  {
+    "revision": "45fffab86fffee3ca803ab56391fc480",
+    "url": "/a9f32963a74e691ab256e496e9d1d6ef31c7b1ff/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/a9f32963a74e691ab256e496e9d1d6ef31c7b1ff/precache-manifest.30b5bfaae4e013f22f56a6b1f393f257.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/a9f32963a74e691ab256e496e9d1d6ef31c7b1ff/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
